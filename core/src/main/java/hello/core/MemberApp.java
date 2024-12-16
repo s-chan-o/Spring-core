ec84bb73b7c765db053ed8723cc376aaade39792
김승찬 <s24041@gsm.hs.kr>
2024-12-16 14:50:56 +0900

    관심사 분리

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -1,14 +1,13 @@
 package hello.core;
 
-import hello.core.member.Grade;
-import hello.core.member.Member;
-import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import hello.core.member.*;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+      //MemberService memberService = new MemberServiceImpl();
         Member member = new Member("memberA", 1L, Grade.VIP);
         memberService.join(member);
 
