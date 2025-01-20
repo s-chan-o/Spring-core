d07f6cb23a85f04b87a0c819fa844357f536154d
김승찬 <s24041@gsm.hs.kr>
2025-01-21 00:31:23 +0900

    싱글톤 방식의 주의점 문제 해결

--- core/src/test/java/hello/core/singleton/StatefulService.java
+++ core/src/test/java/hello/core/singleton/StatefulService.java
@@ -4,12 +4,14 @@ public class StatefulService {
 
     private int price; //상태를 유지하는 필드 10000 -> 20000
 
-    public void order(String name, int price){
+    public int order(String name, int price){
         System.out.println("name = " + name + ", price = " + price);
-        this.price = price; //여기가 문제!
-    }
-
-    public int getPrice(){
+        //this.price = price; //여기가 문제!
         return price;
     }
+
+    //public int getPrice(){
+        //return price;
+
+    //}
 }
